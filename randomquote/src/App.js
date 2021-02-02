75457c897f75d957270b87a8c3c5508c69b13c3b
Umesh Raghav <umeshraghav55@gmail.com>
2021-02-02 23:21:39 +0530

    First Assignment done

--- randomquote/src/App.js
+++ randomquote/src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react'
+ 
 import './App.css';
+ 
+const arr = [[" Alistair Cockburn", "A user story is a promise for a conversation"],["Kent Beck","Extreme Programming Explained"],["Ron Jeffries","Three Cs"],[" Mike Cohn ","User Stories Applied: For Agile Software Development"]]
+class App extends React.Component {
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  constructor(props){
+    super(props);
+    let ind=Math.floor(Math.random()* arr.length)
+    this.state={
+      quote: arr[ind][1],
+      author: arr[ind][0]
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick(){
+    let ind=Math.floor(Math.random()* arr.length)
+    return(    
+      this.setState({
+        quote:this.state.quote=arr[ind][1],
+        author:this.state.quote=arr[ind][0]
+      })
+      
+      
+      )
+  }
+  render(){
+    return (
+      <div className="container">
+              
+              
+                <div className="wrapper-box" id="quote-box">
+                  <h3 className="quote" id="text"> "{this.state.quote}"</h3>
+                  <p id="author" className="author">  - {this.state.author}</p>
+                  <button className="btn" onClick={this.handleClick}  id="new-quote">New Quote</button>
+                  <a href='https://twitter.com/intent/tweet?' className="lnk" target="_blank" id="tweet-quote">tweet-quote</a>
+                </div>
+                
+              
+      </div>
+    )
+  }
 }
 
 export default App;
